7ed266171b0169ae035479542a2ab96b2f2c3633
Caiorossi00 <caio_rossi56@hotmail.com>
2025-09-04 17:45:56 -0300

    Initialize Go module and implement basic CRUD operations for jogos

---
+++ Go-gin/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type Jogo struct {
+	ID     string `json:"id"`
+	Titulo string `json:"titulo"`
+	Plataforma string `json:"plataforma"`
+	Nota float32 `json:"nota"`
+}
+
+var jogos = []Jogo{
+	{ID: "1", Titulo: "Dark Souls Remastered", Plataforma: "PC", Nota: 9.3},
+	{ID: "2", Titulo: "Bloodborne", Plataforma: "PlayStation 4", Nota: 9.3},
+	{ID: "3", Titulo: "Elden Ring", Plataforma: "PC", Nota: 9.4},
+	{ID: "4", Titulo: "Sekiro: Shadows Die Twice", Plataforma: "PlayStation 4", Nota: 9.5},
+	{ID: "5", Titulo: "Demon's Souls", Plataforma: "PlayStation 5", Nota: 9.4},
+	{ID: "6", Titulo: "Dark Souls II", Plataforma: "PlayStation 3", Nota: 10.0},
+
+}
+
+func getJogos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, jogos)
+}
+
+func getJogosByID(c *gin.Context) {
+	id:= c.Param("id")
+
+	for _, a := range jogos {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "jogo não encontrado"})
+}
+
+func postJogos(c *gin.Context) {
+	var novoJogo Jogo
+
+	if err := c.BindJSON(&novoJogo); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos"})
+		return
+	}
+
+	lastID := len(jogos) + 1
+	novoJogo.ID = strconv.Itoa(lastID)
+
+	jogos = append(jogos, novoJogo)
+
+	c.IndentedJSON(http.StatusCreated, novoJogo)
+}
+
+func updateJogos(c *gin.Context) {
+	id := c.Param("id")
+	var jogoAtualizado Jogo
+
+	if err := c.BindJSON(&jogoAtualizado); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos"})
+		return
+	}
+
+	for i, a := range jogos {
+		if a.ID == id {
+			jogoAtualizado.ID = id
+			jogos[i] = jogoAtualizado
+			c.IndentedJSON(http.StatusOK, jogoAtualizado)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "jogo não encontrado"})
+}
+
+func deleteJogos(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range jogos {
+		if a.ID == id {
+			jogos = append(jogos[:i], jogos[i+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "jogo deletado"})
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "jogo não encontrado"})
+}
+
+func main(){
+	router := gin.Default()
+
+	router.GET("/jogos", getJogos)
+	router.GET("/jogos/:id", getJogosByID)
+	router.POST("/jogos", postJogos)
+	router.PUT("/jogos/:id", updateJogos)
+	router.DELETE("/jogos/:id", deleteJogos)		
+
+	router.Run("localhost:8080")
+}
+
